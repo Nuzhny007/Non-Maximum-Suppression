cbd7dba2239a243cc6bf6f3a3b458472052d6446
Nuzhny007 <nuzhny@mail.ru>
2017-05-09 07:43:17 +0300

    Added NMS with detection scores

--- main.cpp
+++ main.cpp
@@ -1,100 +1,58 @@
 #include <iostream>
-
-#include <opencv2/opencv.hpp>
-
-void nms(
-        const std::vector<cv::Rect>& srcRects,
-        std::vector<cv::Rect>& resRects,
-        float thresh,
-        int neighbors = 0
-        )
-{
-    resRects.clear();
-
-    const size_t size = srcRects.size();
-    if (!size)
-    {
-        return;
-    }
-
-    // Sort the bounding boxes by the bottom - right y - coordinate of the bounding box
-    std::multimap<int, size_t> idxs;
-    for (size_t i = 0; i < size; ++i)
-    {
-        idxs.insert(std::pair<int, size_t>(srcRects[i].br().y, i));
-    }
-
-    // keep looping while some indexes still remain in the indexes list
-    while (idxs.size() > 0)
-    {
-        // grab the last rectangle
-        auto lastElem = --std::end(idxs);
-        const cv::Rect& rect1 = srcRects[lastElem->second];
-
-        int neigborsCount = 0;
-
-        idxs.erase(lastElem);
-
-        for (auto pos = std::begin(idxs); pos != std::end(idxs); )
-        {
-            // grab the current rectangle
-            const cv::Rect& rect2 = srcRects[pos->second];
-
-            float intArea = (rect1 & rect2).area();
-            float unionArea = rect1.area() + rect2.area() - intArea;
-            float overlap = intArea / unionArea;
-
-            // if there is sufficient overlap, suppress the current bounding box
-            if (overlap > thresh)
-            {
-                pos = idxs.erase(pos);
-                ++neigborsCount;
-            }
-            else
-            {
-                ++pos;
-            }
-        }
-        if (neigborsCount >= neighbors)
-        {
-            resRects.push_back(rect1);
-        }
-    }
-}
-
+#include "nms.h"
 
 /**
-*******************************************************************************
-*
-*   main
-*
-*******************************************************************************
-*/
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char* argv[])
 {
     std::vector<cv::Rect> srcRects;
+    std::vector<float> scores;
 
-    /*
-    // Test 1
-    srcRects.push_back(cv::Rect(cv::Point(114, 60), cv::Point(178, 124)));
-    srcRects.push_back(cv::Rect(cv::Point(120, 60), cv::Point(184, 124)));
-    srcRects.push_back(cv::Rect(cv::Point(114, 66), cv::Point(178, 130)));*/
+    int testInd = 3;
 
+    switch (testInd)
+    {
+    case 0:
+        srcRects.push_back(cv::Rect(cv::Point(114, 60), cv::Point(178, 124)));
+        srcRects.push_back(cv::Rect(cv::Point(120, 60), cv::Point(184, 124)));
+        srcRects.push_back(cv::Rect(cv::Point(114, 66), cv::Point(178, 130)));
+        break;
 
-    // Test 2
-    /*srcRects.push_back(cv::Rect(cv::Point(12, 84), cv::Point(140, 212)));
-    srcRects.push_back(cv::Rect(cv::Point(24, 84), cv::Point(152, 212)));
-    srcRects.push_back(cv::Rect(cv::Point(12, 96), cv::Point(140, 224)));
-    srcRects.push_back(cv::Rect(cv::Point(36, 84), cv::Point(164, 212)));
-    srcRects.push_back(cv::Rect(cv::Point(24, 96), cv::Point(152, 224)));
-    srcRects.push_back(cv::Rect(cv::Point(24, 108), cv::Point(152, 236)));*/
+    case 1:
+        srcRects.push_back(cv::Rect(cv::Point(12, 84), cv::Point(140, 212)));
+        srcRects.push_back(cv::Rect(cv::Point(24, 84), cv::Point(152, 212)));
+        srcRects.push_back(cv::Rect(cv::Point(12, 96), cv::Point(140, 224)));
+        srcRects.push_back(cv::Rect(cv::Point(36, 84), cv::Point(164, 212)));
+        srcRects.push_back(cv::Rect(cv::Point(24, 96), cv::Point(152, 224)));
+        srcRects.push_back(cv::Rect(cv::Point(24, 108), cv::Point(152, 236)));
+        break;
 
-    // Test 3
-    srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
-    srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 100)));
-    srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
-    srcRects.push_back(cv::Rect(cv::Point(84, 48), cv::Point(212, 176)));
-    srcRects.push_back(cv::Rect(cv::Point(1, 1), cv::Point(40, 40)));
+    case 2:
+        srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
+        srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 100)));
+        srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
+        srcRects.push_back(cv::Rect(cv::Point(84, 48), cv::Point(212, 176)));
+        srcRects.push_back(cv::Rect(cv::Point(1, 1), cv::Point(40, 40)));
+        break;
+
+    case 3:
+        srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
+        scores.push_back(0.7f);
+
+        srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 110)));
+        scores.push_back(0.5f);
+
+        srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
+        scores.push_back(0.3f);
+
+        srcRects.push_back(cv::Rect(cv::Point(84, 58), cv::Point(212, 186)));
+        scores.push_back(0.4f);
+        break;
+    }
 
     cv::Size size(0, 0);
     for (const auto& r : srcRects)
@@ -108,10 +66,22 @@ int main(int argc, char* argv[])
     cv::Mat imgCopy = img.clone();
 
 
-
-    for (auto r : srcRects)
+    if (srcRects.size() == scores.size())
     {
-        cv::rectangle(img, r, cv::Scalar(0, 0, 255), 2);
+        for (size_t i = 0; i < srcRects.size(); ++i)
+        {
+            const auto& r = srcRects[i];
+
+            cv::rectangle(img, r, cv::Scalar(0, 0, 255), 2);
+            cv::putText(img, std::to_string(scores[i]), cv::Point(r.x + 2, r.y + r.height - 4), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200), 1, cv::LINE_8, false);
+        }
+    }
+    else
+    {
+        for (auto r : srcRects)
+        {
+            cv::rectangle(img, r, cv::Scalar(0, 0, 255), 2);
+        }
     }
 
     cv::namedWindow("before", cv::WINDOW_NORMAL);
@@ -119,7 +89,14 @@ int main(int argc, char* argv[])
     cv::waitKey(1);
 
     std::vector<cv::Rect> resRects;
-    nms(srcRects, resRects, 0.3f, 1);
+    if (srcRects.size() == scores.size())
+    {
+        nms2(srcRects, scores, resRects, 0.3f, 1);
+    }
+    else
+    {
+        nms(srcRects, resRects, 0.3f, 1);
+    }
 
     for (auto r : resRects)
     {
