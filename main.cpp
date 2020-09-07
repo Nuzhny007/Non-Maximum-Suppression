198fede2981431dacdbc1929e3ff5ebb76dc7e35
Nuzhny007 <nuzhny@mail.ru>
2020-09-07 06:24:24 +0300

    Added soft nms

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "nms.h"
 
+///
+enum class TestCases
+{
+	OnlyRectangles0,
+	OnlyRectangles1,
+	OnlyRectangles2,
+	RectanglesScores,
+	RectanglesScoresMinSum,
+	SoftNMS
+};
+
 /**
  * @brief main
  * @param argc
@@ -12,63 +23,77 @@ int main(int argc, char* argv[])
     std::vector<cv::Rect> srcRects;
     std::vector<float> scores;
 
-    int testInd = 4;
+	TestCases testInd = TestCases::SoftNMS;
     float minScoresSum = 0;
 
     switch (testInd)
     {
-    case 0:  // Only rectangles
-        srcRects.push_back(cv::Rect(cv::Point(114, 60), cv::Point(178, 124)));
-        srcRects.push_back(cv::Rect(cv::Point(120, 60), cv::Point(184, 124)));
-        srcRects.push_back(cv::Rect(cv::Point(114, 66), cv::Point(178, 130)));
+    case TestCases::OnlyRectangles0:  // Only rectangles
+        srcRects.emplace_back(cv::Point(114, 60), cv::Point(178, 124));
+        srcRects.emplace_back(cv::Point(120, 60), cv::Point(184, 124));
+        srcRects.emplace_back(cv::Point(114, 66), cv::Point(178, 130));
         break;
 
-    case 1:  // Only rectangles
-        srcRects.push_back(cv::Rect(cv::Point(12, 84), cv::Point(140, 212)));
-        srcRects.push_back(cv::Rect(cv::Point(24, 84), cv::Point(152, 212)));
-        srcRects.push_back(cv::Rect(cv::Point(12, 96), cv::Point(140, 224)));
-        srcRects.push_back(cv::Rect(cv::Point(36, 84), cv::Point(164, 212)));
-        srcRects.push_back(cv::Rect(cv::Point(24, 96), cv::Point(152, 224)));
-        srcRects.push_back(cv::Rect(cv::Point(24, 108), cv::Point(152, 236)));
+    case TestCases::OnlyRectangles1:  // Only rectangles
+        srcRects.emplace_back(cv::Point(12, 84), cv::Point(140, 212));
+        srcRects.emplace_back(cv::Point(24, 84), cv::Point(152, 212));
+        srcRects.emplace_back(cv::Point(12, 96), cv::Point(140, 224));
+        srcRects.emplace_back(cv::Point(36, 84), cv::Point(164, 212));
+        srcRects.emplace_back(cv::Point(24, 96), cv::Point(152, 224));
+        srcRects.emplace_back(cv::Point(24, 108), cv::Point(152, 236));
         break;
 
-    case 2:  // Only rectangles
-        srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
-        srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 100)));
-        srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
-        srcRects.push_back(cv::Rect(cv::Point(84, 48), cv::Point(212, 176)));
-        srcRects.push_back(cv::Rect(cv::Point(1, 1), cv::Point(40, 40)));
+    case TestCases::OnlyRectangles2:  // Only rectangles
+        srcRects.emplace_back(cv::Point(12, 30), cv::Point(76, 94));
+        srcRects.emplace_back(cv::Point(12, 36), cv::Point(76, 100));
+        srcRects.emplace_back(cv::Point(72, 36), cv::Point(200, 164));
+        srcRects.emplace_back(cv::Point(84, 48), cv::Point(212, 176));
+        srcRects.emplace_back(cv::Point(1, 1), cv::Point(40, 40));
         break;
 
-    case 3:  // Rectangles and scores
-        srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
-        scores.push_back(0.7f);
+    case TestCases::RectanglesScores:  // Rectangles and scores
+        srcRects.emplace_back(cv::Point(12, 30), cv::Point(76, 94));
+        scores.emplace_back(0.7f);
 
-        srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 110)));
-        scores.push_back(0.5f);
+        srcRects.emplace_back(cv::Point(12, 36), cv::Point(76, 110));
+        scores.emplace_back(0.5f);
 
-        srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
-        scores.push_back(0.3f);
+        srcRects.emplace_back(cv::Point(72, 36), cv::Point(200, 164));
+        scores.emplace_back(0.3f);
 
-        srcRects.push_back(cv::Rect(cv::Point(84, 58), cv::Point(212, 186)));
-        scores.push_back(0.4f);
+        srcRects.emplace_back(cv::Point(84, 58), cv::Point(212, 186));
+        scores.emplace_back(0.4f);
         break;
 
-    case 4:  // Rectangles, scores and minimum score sum
-        srcRects.push_back(cv::Rect(cv::Point(12, 30), cv::Point(76, 94)));
-        scores.push_back(0.7f);
+    case TestCases::RectanglesScoresMinSum:  // Rectangles, scores and minimum score sum
+        srcRects.emplace_back(cv::Point(12, 30), cv::Point(76, 94));
+        scores.emplace_back(0.7f);
 
-        srcRects.push_back(cv::Rect(cv::Point(12, 36), cv::Point(76, 110)));
-        scores.push_back(0.5f);
+        srcRects.emplace_back(cv::Point(12, 36), cv::Point(76, 110));
+        scores.emplace_back(0.5f);
 
-        srcRects.push_back(cv::Rect(cv::Point(72, 36), cv::Point(200, 164)));
-        scores.push_back(0.3f);
+        srcRects.emplace_back(cv::Point(72, 36), cv::Point(200, 164));
+        scores.emplace_back(0.3f);
 
-        srcRects.push_back(cv::Rect(cv::Point(84, 58), cv::Point(212, 186)));
-        scores.push_back(0.4f);
+        srcRects.emplace_back(cv::Point(84, 58), cv::Point(212, 186));
+        scores.emplace_back(0.4f);
 
         minScoresSum = 0.8f;
         break;
+
+	case TestCases::SoftNMS: // Soft nms
+		srcRects.emplace_back(cv::Point(12, 30), cv::Point(76, 94));
+		scores.emplace_back(0.9f);
+
+		srcRects.emplace_back(cv::Point(12, 36), cv::Point(76, 110));
+		scores.emplace_back(0.7f);
+
+		srcRects.emplace_back(cv::Point(72, 36), cv::Point(200, 164));
+		scores.emplace_back(0.6f);
+
+		srcRects.emplace_back(cv::Point(84, 58), cv::Point(212, 186));
+		scores.emplace_back(0.5f);
+		break;
     }
 
     cv::Size size(0, 0);
@@ -108,26 +133,37 @@ int main(int argc, char* argv[])
         }
     }
 
-    cv::namedWindow("before", cv::WINDOW_NORMAL);
+    cv::namedWindow("before", cv::WINDOW_AUTOSIZE);
     cv::imshow("before", img);
     cv::waitKey(1);
 
     std::vector<cv::Rect> resRects;
-    if (srcRects.size() == scores.size())
-    {
-        nms2(srcRects, scores, resRects, 0.3f, 1, minScoresSum);
-    }
-    else
-    {
-        nms(srcRects, resRects, 0.3f, 1);
-    }
+	std::vector<float> resScores;
+    
+	switch (testInd)
+	{
+	case TestCases::OnlyRectangles0:
+	case TestCases::OnlyRectangles1:
+	case TestCases::OnlyRectangles2:
+		nms(srcRects, resRects, 0.3f, 1);
+		break;
+
+	case TestCases::RectanglesScores:
+	case TestCases::RectanglesScoresMinSum:
+		nms2(srcRects, scores, resRects, 0.3f, 1, minScoresSum);
+		break;
+
+	case TestCases::SoftNMS:
+		soft_nms(srcRects, scores, resRects, resScores, 0.3f, 0.5f, Methods::GaussNMS, 0.5f);
+		break;
+	}
 
     for (auto r : resRects)
     {
         cv::rectangle(imgCopy, r, cv::Scalar(0, 255, 0), 2);
     }
 
-    cv::namedWindow("after", cv::WINDOW_NORMAL);
+    cv::namedWindow("after", cv::WINDOW_AUTOSIZE);
     cv::imshow("after", imgCopy);
 
     cv::waitKey(0);
--- nms.h
+++ nms.h
@@ -10,26 +10,22 @@
  * @param thresh
  * @param neighbors
  */
-inline void nms(
-        const std::vector<cv::Rect>& srcRects,
-        std::vector<cv::Rect>& resRects,
-        float thresh,
-        int neighbors = 0
-        )
+inline void nms(const std::vector<cv::Rect>& srcRects,
+                std::vector<cv::Rect>& resRects,
+                float thresh,
+                int neighbors = 0)
 {
     resRects.clear();
 
     const size_t size = srcRects.size();
     if (!size)
-    {
         return;
-    }
 
     // Sort the bounding boxes by the bottom - right y - coordinate of the bounding box
     std::multimap<int, size_t> idxs;
     for (size_t i = 0; i < size; ++i)
     {
-        idxs.insert(std::pair<int, size_t>(srcRects[i].br().y, i));
+        idxs.emplace(srcRects[i].br().y, i);
     }
 
     // keep looping while some indexes still remain in the indexes list
@@ -48,7 +44,7 @@ inline void nms(
             // grab the current rectangle
             const cv::Rect& rect2 = srcRects[pos->second];
 
-            float intArea = (rect1 & rect2).area();
+            float intArea = static_cast<float>((rect1 & rect2).area());
             float unionArea = rect1.area() + rect2.area() - intArea;
             float overlap = intArea / unionArea;
 
@@ -64,9 +60,7 @@ inline void nms(
             }
         }
         if (neigborsCount >= neighbors)
-        {
             resRects.push_back(rect1);
-        }
     }
 }
 
@@ -79,22 +73,18 @@ inline void nms(
  * @param thresh
  * @param neighbors
  */
-inline void nms2(
-        const std::vector<cv::Rect>& srcRects,
-        const std::vector<float>& scores,
-        std::vector<cv::Rect>& resRects,
-        float thresh,
-        int neighbors = 0,
-        float minScoresSum = 0.f
-        )
+inline void nms2(const std::vector<cv::Rect>& srcRects,
+                 const std::vector<float>& scores,
+                 std::vector<cv::Rect>& resRects,
+                 float thresh,
+                 int neighbors = 0,
+                 float minScoresSum = 0.f)
 {
     resRects.clear();
 
     const size_t size = srcRects.size();
     if (!size)
-    {
         return;
-    }
 
     assert(srcRects.size() == scores.size());
 
@@ -102,7 +92,7 @@ inline void nms2(
     std::multimap<float, size_t> idxs;
     for (size_t i = 0; i < size; ++i)
     {
-        idxs.insert(std::pair<float, size_t>(scores[i], i));
+        idxs.emplace(scores[i], i);
     }
 
     // keep looping while some indexes still remain in the indexes list
@@ -122,7 +112,7 @@ inline void nms2(
             // grab the current rectangle
             const cv::Rect& rect2 = srcRects[pos->second];
 
-            float intArea = (rect1 & rect2).area();
+            float intArea = static_cast<float>((rect1 & rect2).area());
             float unionArea = rect1.area() + rect2.area() - intArea;
             float overlap = intArea / unionArea;
 
@@ -138,10 +128,119 @@ inline void nms2(
                 ++pos;
             }
         }
-        if (neigborsCount >= neighbors &&
-                scoresSum >= minScoresSum)
-        {
+        if (neigborsCount >= neighbors && scoresSum >= minScoresSum)
             resRects.push_back(rect1);
-        }
     }
+}
+
+///
+enum class Methods
+{
+	ClassicNMS,
+	LinearNMS,
+	GaussNMS
+};
+
+/**
+ * @brief nms2
+ * Non maximum suppression with detection scores
+ * @param srcRects
+ * @param scores
+ * @param resRects
+ * @param thresh
+ */
+inline void soft_nms(const std::vector<cv::Rect>& srcRects,
+                     const std::vector<float>& scores,
+                     std::vector<cv::Rect>& resRects,
+                     std::vector<float>& resScores,
+                     float iou_thresh,
+                     float score_thresh,
+                     Methods method,
+                     float sigma = 0.5f)
+{
+	resRects.clear();
+
+	const size_t size = srcRects.size();
+	if (!size)
+		return;
+
+	assert(srcRects.size() == scores.size());
+
+	// Sort the bounding boxes by the detection score
+	std::multimap<float, size_t> idxs;
+	for (size_t i = 0; i < size; ++i)
+	{
+		if (scores[i] >= score_thresh)
+			idxs.emplace(scores[i], i);
+	}
+
+	if (resRects.capacity() < idxs.size())
+	{
+		resRects.reserve(idxs.size());
+		resScores.reserve(idxs.size());
+	}
+
+	// keep looping while some indexes still remain in the indexes list
+	while (idxs.size() > 0)
+	{
+		// grab the last rectangle
+		auto lastElem = --std::end(idxs);
+		const cv::Rect& rect1 = srcRects[lastElem->second];
+
+		if (lastElem->first >= score_thresh)
+		{
+			resRects.push_back(rect1);
+			resScores.push_back(lastElem->first);
+		}
+		else
+		{
+			break;
+		}
+		idxs.erase(lastElem);
+
+		for (auto pos = std::begin(idxs); pos != std::end(idxs); )
+		{
+			// grab the current rectangle
+			const cv::Rect& rect2 = srcRects[pos->second];
+
+			float intArea = static_cast<float>((rect1 & rect2).area());
+			float unionArea = rect1.area() + rect2.area() - intArea;
+			float overlap = intArea / unionArea;
+
+			// if there is sufficient overlap, suppress the current bounding box
+			if (overlap > iou_thresh)
+			{
+				float weight = 1.f;
+				switch (method)
+				{
+				case Methods::ClassicNMS:
+					weight = 0;
+					break;
+				case Methods::LinearNMS:
+					weight = 1.f - overlap;
+					break;
+				case Methods::GaussNMS:
+					weight = exp(-(overlap * overlap) / sigma);
+					break;
+				}
+
+				float newScore = pos->first * weight;
+				if (newScore < score_thresh)
+				{
+					pos = idxs.erase(pos);
+				}
+				else
+				{
+					auto n = idxs.extract(pos);
+					n.key() = newScore;
+					idxs.insert(std::move(n));
+					++pos;
+				}
+			}
+			else
+			{
+				++pos;
+			}
+		}
+	}
 }
